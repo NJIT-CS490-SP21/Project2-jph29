b0a1480577dc5b8b0a5bacc19a2caae6c422e6fd
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-02-25 20:15:39 +0000

    Initial commit of tic tac toe board

---
+++ src/Board.js
@@ -0,0 +1,18 @@
+import './Board.css';
+
+export function MakeBoard(){
+  return(
+<div class="board">
+  <div class="box">X</div>
+  <div class="box">O</div>
+  <div class="box">O</div>
+  <div class="box">O</div>
+  <div class="box">X</div>
+  <div class="box">O</div>
+  <div class="box">O</div>
+  <div class="box">X</div>
+  <div class="box">X</div>
+</div>);
+}
+
+
