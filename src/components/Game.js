f0e528ab56588a93078c80d767029d5acfab79b3
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-02-27 02:57:32 +0000

    Functional Board and Game;Need to finish io socket, connecting two browsers;Need to finish Database setup for leaderboard;Need to setup user-name input;Lastly, Need to deploy to Heroku

---
+++ src/components/Game.js
@@ -0,0 +1,110 @@
+import React, { useState, useRef, useEffect  } from "react";
+import { calculateWinner } from "../helper";
+import MakeBoard from "./Board";
+import io from 'socket.io-client';
+
+const socket = io(); // Connects to socket connection
+function App() {
+  const [messages, setMessages] = useState([]); // State variable, list of messages
+  const inputRef = useRef(null); // Reference to <input> element
+
+  function onClickButton() {
+    if (inputRef != null) {
+      const message = inputRef.current.value;
+      // If your own client sends a message, we add it to the list of messages to 
+      // render it on the UI.
+      setMessages(prevMessages => [...prevMessages, message]);
+      socket.emit('chat', { message: message });
+    }
+  }
+
+  // The function inside useEffect is only run whenever any variable in the array
+  // (passed as the second arg to useEffect) changes. Since this array is empty
+  // here, then the function will only run once at the very beginning of mounting.
+  useEffect(() => {
+    // Listening for a chat event emitted by the server. If received, we
+    // run the code in the function that is passed in as the second arg
+    socket.on('chat', (data) => {
+      console.log('Chat event received!');
+      console.log(data);
+      // If the server sends a message (on behalf of another client), then we
+      // add it to the list of messages to render it on the UI.
+      setMessages(prevMessages => [...prevMessages, data.message]);
+    });
+  }, []);
+}
+
+
+
+
+const Game = () => {
+  const [gameState, setGameState] = useState([]); // State variable, list of messages
+  const inputRef = useRef(null);
+  
+  const onClickButton = () => {
+    if (inputRef != null){
+      setGameState(prevGameState => [...prevGameState, gameState]);
+      socket.emit('game', { gameState: gameState});
+    }
+  }
+  useEffect(() => {
+    socket.on('game', (data) => {
+      console.log('Game State updated')
+      console.log(data);
+      
+      setGameState(prevGameState => [...prevGameState, data.game]);
+    });
+  }, []);
+  
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+    // select square
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move #${move}` : "Go to Start";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <h1>Jaymee's Tic Tac Toe Board</h1>
+      <MakeBoard squares={history[stepNumber]} onClick={handleClick}/>
+      
+      <div className="info-wrapper">
+        <div>
+          <h3>History</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? "Winner: " + winner : ""}</h3>
+        <h3>{!winner && history.length===10? "The Game Is a Draw": "Next Player: " + xO}</h3>
+      </div>
+   </> 
+  );
+};
+
+export default Game;
