f11fefd6ee020d5ca00d5411ad014c76e7da085e
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-02-27 22:10:08 +0000

    updated board to reference square

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Square from "./Square";
-
+//passes back to square
 const MakeBoard = ({ squares, onClick }) => (
   <div className="board">
     {squares.map((square, i) => (
