e323471c713f9d0479542a0221263cae52746d96
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-03-01 18:26:52 +0000

    code cleanup

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,45 +10,54 @@ const Game = () => {
   const [gameStates, setGameStates] = useState([]); // State variable, list of messages
   const inputRef = useRef(null);
   
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [boardHistory, setBoardHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(history[stepNumber]);
+  const winner = calculateWinner(boardHistory[stepNumber]);
   const xO = xIsNext ? "X" : "O";
 
   const handleClick = (i) => {
-    const historyPoint = history.slice(0, stepNumber + 1);
-    const current = historyPoint[stepNumber];
-    const squares = [...current];
+    const historyPoint = boardHistory.slice(0, stepNumber + 1);
+    const currentBoard = historyPoint[stepNumber];
+    const squares = [...currentBoard];
     // return if won or occupied
     if (winner || squares[i]) return;
     // select square
     squares[i] = xO;
-    setHistory([...historyPoint, squares]);
+    setBoardHistory([...historyPoint, squares]);
     setStepNumber(historyPoint.length);
     setXisNext(!xIsNext);
     //update board for multiple browsers
     //#TODO: Multiplayer funcitonality
     if (inputRef != null){
-      const gameState = squares;
-      setGameStates(gameState => [...gameState]);
-      socket.emit('game', {gameState: gameState});
-      return gameStates;
+      socket.emit('game', {boardHistory: boardHistory});
+      return historyPoint[stepNumber];
     }
   };
    // The function inside useEffect is only run whenever any variable in the array
   // (passed as the second arg to useEffect) changes. Since this array is empty
   // here, then the function will only run once at the very beginning of mounting.
-  useEffect(() => {
+  useEffect((i) => {
+    
     // Listening for a chat event emitted by the server. If received, we
     // run the code in the function that is passed in as the second arg
     socket.on('game', (data) => {
-      console.log('Game State Updated!');
-      console.log(data);
+    const historyPoint = boardHistory.slice(0, stepNumber + 1);
+    const currentBoard = historyPoint[stepNumber];
+    const squares = [...currentBoard];
+    
+    squares[i] = xO;
+    setBoardHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+    if (winner || squares[i]) return;
+    console.log('Game State Updated!');
+    console.log(data);
       // If the server sends a message (on behalf of another client), then we
       // add it to the list of messages to render it on the UI.
-      setGameStates(gameState => [...gameState]);
-      return gameStates;
+    
+    socket.emit('game', { boardHistory: boardHistory});
+    return data.historyPoint[stepNumber];
     });
   }, []);
 
@@ -58,7 +67,7 @@ const Game = () => {
   };
 
   const renderMoves = () =>
-    history.map((_step, move) => {
+    boardHistory.map((_step, move) => {
       const destination = move ? `Go to move #${move}` : "Go to Start";
       return (
         <li key={move}>
@@ -70,16 +79,14 @@ const Game = () => {
   return (
     <>
       <h1>Jaymee's Tic Tac Toe Board</h1>
-      <MakeBoard squares={history[stepNumber]} onClick={handleClick}/>
-      
-      
+      <MakeBoard squares={boardHistory[stepNumber]} onClick={handleClick}/>
       <div className="info-wrapper">
         <div>
           <h3>Match History</h3>
           {renderMoves()}
         </div>
         <h3>{winner ? "Winner: " + winner : ""}</h3>
-        <h3>{!winner && history.length===10? "The Game Is a Draw": "Next Player: " + xO}</h3>
+        <h3>{!winner && boardHistory.length===10? "The Game Is a Draw": "Next Player: " + xO}</h3>
       </div>
    </> 
   );
