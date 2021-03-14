671ed75e0cbe821ec03f37e28073beb12c628a53
EC2 Default User <ec2-user@ip-172-31-13-127.ec2.internal>
2021-03-14 05:49:24 +0000

    Pylint and Eslint tasks are completed

--- app.py
+++ app.py
@@ -1,102 +1,98 @@
+"""
+Back-End file that communicates with Postgress-Database, and listens/emits socket data
+"""
+
 import os
 from flask import Flask, send_from_directory, json, session
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
-
+import models
 load_dotenv(find_dotenv())
 
-app = Flask(__name__, static_folder='./build/static')
+APP = Flask(__name__, static_folder='./build/static')
 # Point SQLAlchemy to your Heroku database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # Gets rid of a warning
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
-# IMPORTANT: This must be AFTER creating db variable to prevent
+# IMPORTANT: This must be AFTER creating DB variable to prevent
 # circular import issues
-import models
-db.create_all()
 
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+DB.create_all()
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
+CORS = CORS(APP, resources={r"/*": {"origins": "*"}})
+
+SOCKETIO = SocketIO(
+    APP,
+    CORS_allowed_origins="*",
     json=json,
     manage_session=False
 )
 
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
+@APP.route('/', defaults={"filename": "index.html"})
+@APP.route('/<path:filename>')
 def index(filename):
+    '''Function to build the filename'''
     return send_from_directory('./build', filename)
 
-# When a client connects from this Socket connection, this function is run
-@socketio.on('connect')
+@SOCKETIO.on('connect')
 def on_connect():
+    '''Function to show that a user has connected'''
     print('User connected!')
 
-# When a client disconnects from this Socket connection, this function is run
-@socketio.on('disconnect')
+@SOCKETIO.on('disconnect')
 def on_disconnect():
+    '''Function to show that a user has disconnected'''
     print('User disconnected!')
 
-# When a client emits the event 'chat' to the server, this function is run
-# 'chat' is a custom event name that we just decided
-@socketio.on('game')
-def on_chat(data): # data is whatever arg you pass in your emit call on client
+@SOCKETIO.on('game')
+def on_chat(data):
+    '''Function to show that the game state has been updated'''
     print(str(data))
-    # This emits the game event from the server to all clients except for
-    # the client that emmitted the event that triggered this function
-    socketio.emit('game',  data, broadcast=True, include_self=False)
+    SOCKETIO.emit('game', data, broadcast=True, include_self=False)
 
-@socketio.on('logIn')
+@SOCKETIO.on('logIn')
 def on_log(data):
+    '''Function to show that a user has logged in'''
     print(str(data))
-    exists = db.session.query(db.exists().where(models.Gamer.username == data['userName'])).scalar()
+    exists = DB.session.query(DB.exists().where(models.Gamer.username == data['userName'])).scalar()
     if not exists:
         new_user = models.Gamer(username=data['userName'], gameswon=0)
-        db.session.add(new_user)
-        db.session.commit()
-    #all_people = models.Gamer.query.all()
-    all_scores = db.session.query(models.Gamer).order_by(models.Gamer.gameswon.desc()).all()
+        DB.session.add(new_user)
+        DB.session.commit()
+    all_scores = DB.session.query(models.Gamer).order_by(models.Gamer.gameswon.desc()).all()
     users = []
     white_space = '             '
-   
     for person in all_scores:
         users.append(person.username + white_space + str(person.gameswon))
-    #print(models.Gamer)
     print(users)
     print(all_scores)
-    #print(exists)
-    socketio.emit('logIn', data, broadcast=True, include_self=False)
-    socketio.emit('userList',{'users':users})
+    SOCKETIO.emit('logIn', data, broadcast=True, include_self=False)
+    SOCKETIO.emit('userList', {'users':users})
 
-@socketio.on('winner')
+@SOCKETIO.on('winner')
 def on_win(data):
+    '''Function to increment winners score and send latest database to app'''
     print('WIN EVENT RECIEVED!')
     print(str(data))
-    Winner = db.session.query(models.Gamer).filter_by(username =data['winner']).first()  
+    Winner = DB.session.query(models.Gamer).filter_by(username=data['winner']).first()
     Winner.gameswon = Winner.gameswon + 1
-    db.session.commit()
-    
-    all_scores = db.session.query(models.Gamer).order_by(models.Gamer.gameswon.desc()).all()
+    DB.session.commit()
+    all_scores = DB.session.query(models.Gamer).order_by(models.Gamer.gameswon.desc()).all()
     users = []
     white_space = '             '
-   
     for person in all_scores:
         users.append(person.username + white_space + str(person.gameswon))
-    socketio.emit('userList',{'users':users})
-    
-    
-    
-    # Note that we don't call app.run anymore. We call socketio.run with app arg
+    SOCKETIO.emit('userList', {'users':users})
+    # Note that we don't call App.run anymore. We call SOCKETIO.run with App arg
 if __name__ == "__main__":
-    socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    SOCKETIO.run(
+        APP,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
     )
+    
